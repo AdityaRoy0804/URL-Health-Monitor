64c125bf779ca75e1d58d37b9624ebb8b9785e05
AdityaRoy0804 <roy97278@gmail.com>
2026-08-10 18:15:33 +0530

    added scheduler to monitor the URLs at regular interval and write in DB

--- src/main/java/in/akr/URLMonitor/repository/UrlRepository.java
+++ src/main/java/in/akr/URLMonitor/repository/UrlRepository.java
@@ -3,8 +3,11 @@ package in.akr.URLMonitor.repository;
 import in.akr.URLMonitor.entity.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UrlRepository extends JpaRepository<Url,Long> {
     Optional<Url> findByUrl(String url);
+
+    List<Url> findByEnabledTrue();
 }
