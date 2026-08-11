f038d49ed25560b00c54f384f4bc68b2eb50f89c
AdityaRoy0804 <roy97278@gmail.com>
2026-08-11 18:42:30 +0530

    re-configured backend

--- src/main/java/in/akr/URLMonitor/UrlMonitorApplication.java
+++ src/main/java/in/akr/URLMonitor/UrlMonitorApplication.java
@@ -9,6 +9,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class UrlMonitorApplication {
 
 	public static void main(String[] args) {
+		System.out.println("MYSQL_USER = " + System.getenv("MYSQL_USER"));
+		System.out.println("MYSQL_PASSWORD_SET = " +
+				(System.getenv("MYSQL_PASSWORD") != null));
 		SpringApplication.run(UrlMonitorApplication.class, args);
 	}
 }
