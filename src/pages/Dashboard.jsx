5330cf5979bd0f371725cd491f79d33a8ca162eb
AdityaRoy0804 <roy97278@gmail.com>
2026-08-11 23:28:51 +0530

    added axios and simple dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Monitor your URLs and track their health.</p>
+        </div>
+    );
+}
+
+export default Dashboard;
